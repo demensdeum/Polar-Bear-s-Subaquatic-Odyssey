b7a525261f6a2603294f900f0a73d01cb9bb8f3c
Ilia Prokhorov <demensdeum@gmail.com>
2024-06-28 23:58:11 +0400

    Scene prototyping

--- client/src/inGameState.ts
+++ client/src/inGameState.ts
@@ -1,6 +1,8 @@
 import { State } from "./state.js"
 import { debugPrint } from "./runtime.js"
 import { Context } from "./context.js"
+import { DecorControls } from "./decorControls.js"
+import { SceneObjectCommandIdle } from "./sceneObjectCommandIdle.js"
 
 export class InGameState implements State {
     
@@ -18,6 +20,29 @@ export class InGameState implements State {
     initialize(): void {
         debugPrint("InGameState initialized")
         debugPrint(this.context)
+
+        this.context.sceneController.addModelAt(
+            "bear",
+            "com.demensdeum.hero",
+            0,
+            -0.6,
+            -1.6,
+            0,
+            0,
+            0,
+            true,
+            new DecorControls(
+                "hero",
+                new SceneObjectCommandIdle(
+                    "idle",
+                    0
+                ),
+                this.context.sceneController,
+                this.context.sceneController,
+                this.context.sceneController
+            )            
+        )
+
     }
 
     step(): void {
